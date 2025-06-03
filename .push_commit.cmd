b6f56092599bef4042858a3d76506c4bfd7ea67d
brasiledo <lourencd@gmail.com>
2025-06-03 08:25:19 -0400

    Initial commit of PowerShell scripts

--- .push_commit.cmd
+++ .push_commit.cmd
@@ -9,7 +9,7 @@ REM Commit changes
 git commit -m "Initial commit of PowerShell scripts"
 
 REM Add remote (only if not already set)
-git remote add origin https://github.com/yourusername/powershell-activedirectory-tools.git
+git remote add origin https://github.com/brasiledo/powershell-activedirectory-tools.git
 
 REM Set main branch and push
 git branch -M main
